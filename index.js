70cf63e7020ec1a3eeea1ce5c4642eab635762e4
Raymond Mawanda <rmawanda3@gmail.com>
2021-03-18 23:29:45 -0700

    Done with Assignment

---
+++ index.js
@@ -0,0 +1,87 @@
+function add() {
+
+}
+
+function subtract() {
+    
+}
+
+function multiply () {
+
+}
+
+function divide () {
+
+}
+
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+console.log (add(10,50));
+
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+
+console.log (subtract(50,10));
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+console.log (multiply(50,10));
+
+function divide(number1, number2) {
+    return number1 / number2;
+}
+
+console.log (divide(50,10));
+
+function increment(n) {
+    return n + 1 ;
+}
+
+console.log (increment(10));
+
+function decrement(n) {
+    return n - 1 ;
+}
+
+console.log (decrement(10));
+
+function makeInt(x2328) {
+    return 0 ;
+}
+
+console.log (makeInt());
+
+function makeInt(n) {
+    return n ;
+}
+
+console.log (makeInt(10));
+
+function makeInt(n) {
+   if (isNaN('sldkjflksjf'))
+    return n ;
+}
+
+console.log ("true");
+
+function preserveDecimal () {
+return 2.222 ;
+}
+
+console.log(2.222);
+
+function preserveDecimal(n) {
+   if (isNaN('sldkjflksjf'))
+    return n ;
+}
+
+console.log ("true");
+
+
+
+
